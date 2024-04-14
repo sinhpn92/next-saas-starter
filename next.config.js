208de2ba97ce0d49315da782ee66aa3f6132e4c2
Pineapple <pineapple@PineappledeMacBook-Pro.local>
2024-04-15 01:09:28 +0800

    uncomment hero-image

--- next.config.js
+++ next.config.js
@@ -16,9 +16,6 @@ module.exports = withBundleAnalyzer({
   compiler: {
     styledComponents: true,
   },
-  nextjsColorMode: {
-    initialColorMode: 'light',
-  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push({
       test: /\.svg$/,
