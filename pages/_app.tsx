9a09f4341939763d44c2f28234a6a247a81ca5b1
Pineapple <pineapple@PineappledeMacBook-Pro.local>
2024-04-15 14:57:55 +0800

    bruteforce fix cms schema issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,151 @@ import NewsletterModal from 'components/NewsletterModal';
 import WaveCta from 'components/WaveCta';
 import { NewsletterModalContextProvider, useNewsletterModalContext } from 'contexts/newsletter-modal.context';
 import { NavItems } from 'types';
+// import schema from '/.tina/schema';
+import { defineSchema } from 'tinacms';
+
+const schema = defineSchema({
+  collections: [
+    {
+      label: 'Blog Posts',
+      name: 'posts',
+      path: 'posts',
+      fields: [
+        {
+          type: 'string',
+          label: 'Title',
+          name: 'title',
+        },
+        {
+          type: 'string',
+          label: 'Description',
+          name: 'description',
+        },
+        {
+          type: 'string',
+          label: 'Date',
+          name: 'date',
+        },
+        {
+          type: 'string',
+          label: 'Tags',
+          name: 'tags',
+        },
+        {
+          type: 'string',
+          label: 'Image URL',
+          name: 'imageUrl',
+        },
+        {
+          type: 'rich-text',
+          label: 'Blog Post Body',
+          name: 'body',
+          isBody: true,
+          templates: [
+            {
+              name: 'Quote',
+              label: 'Quote',
+              fields: [
+                {
+                  type: 'string',
+                  name: 'content',
+                  label: 'Content',
+                },
+                {
+                  type: 'string',
+                  name: 'author',
+                  label: 'Author',
+                },
+                {
+                  type: 'string',
+                  name: 'cite',
+                  label: 'Cite',
+                },
+              ],
+            },
+            {
+              name: 'ArticleImage',
+              label: 'ArticleImage',
+              fields: [
+                {
+                  type: 'string',
+                  name: 'src',
+                  label: 'Src',
+                },
+                {
+                  type: 'string',
+                  name: 'caption',
+                  label: 'Caption',
+                },
+              ],
+            },
+            {
+              name: 'Code',
+              label: 'Code',
+              fields: [
+                {
+                  type: 'string',
+                  name: 'code',
+                  label: 'Code',
+                },
+                {
+                  type: 'string',
+                  name: 'language',
+                  label: 'Language',
+                },
+                {
+                  type: 'string',
+                  name: 'selectedLines',
+                  label: 'Selected Lines',
+                },
+                {
+                  type: 'boolean',
+                  name: 'withCopyButton',
+                  label: 'With Copy Button',
+                },
+                {
+                  type: 'boolean',
+                  name: 'withLineNumbers',
+                  label: 'With Line Numbers',
+                },
+                {
+                  type: 'string',
+                  name: 'caption',
+                  label: 'Caption',
+                },
+              ],
+            },
+            {
+              name: 'h2',
+              label: 'H2',
+              inline: true,
+              fields: [],
+            },
+            {
+              name: 'h3',
+              label: 'H3',
+              inline: true,
+              fields: [],
+            },
+            {
+              name: 'br',
+              label: 'BR',
+              inline: true,
+              fields: [],
+            },
+            {
+              name: 'p',
+              label: 'P',
+              inline: true,
+              fields: [],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+});
+
 
 const navItems: NavItems = [
   // { title: 'Awesome SaaS Features', href: '/features' },
@@ -54,6 +199,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <TinaEditProvider
           editMode={
             <TinaCMS
+              schema={schema}
               query={pageProps.query}
               variables={pageProps.variables}
               data={pageProps.data}
@@ -64,10 +210,10 @@ function MyApp({ Component, pageProps }: AppProps) {
             >
               {(livePageProps: any) => <Component {...livePageProps} />}
             </TinaCMS>
-          }
+           }
         >
           <Component {...pageProps} />
-        </TinaEditProvider>
+        </TinaEditProvider> 
         <WaveCta />
         <Footer />
       </Providers>
